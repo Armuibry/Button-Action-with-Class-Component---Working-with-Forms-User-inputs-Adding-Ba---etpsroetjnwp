f402ef9b8544886e562927085a7ee43147efdf5f
Md Amir Sohail <52571788+Armuibry@users.noreply.github.com>
2022-09-05 22:14:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+import "./../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+function App() {
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  const [showPara , setShowPara] = useState(false);
+  const renderPara = () => {
+    setShowPara(true)
+  }
+
+  return (
+    <div id="main">
+      { showPara && 
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      }
+      <button id="click" onClick={renderPara}>click</button>
+    </div>
+  );
 }
 
 
 export default App;
-
